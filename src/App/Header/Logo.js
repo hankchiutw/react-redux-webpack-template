e876fee8fe5645e0d4faf4238c24a2ac79c51e23
Hank Chiu <hankchiu.tw@gmail.com>
2017-01-09 17:43:06 +0800

    conditionally render anchorForMobile

--- src/App/Header/Logo.js
+++ src/App/Header/Logo.js
@@ -3,18 +3,22 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const Logo = ({toggleNav}) => {
+const Logo = ({isNavOn, toggleNav}) => {
+    const anchorForMobile = (isNavOn ?
+        <span className='visible-xs-inline pull-left cell' onClick={toggleNav}>
+            <i className='fa fa-long-arrow-left fa-lg'></i>
+        </span>
+        :
+        <span className='visible-xs-inline pull-left cell' onClick={toggleNav}>
+            <i className='fa fa-bars fa-lg'></i>
+        </span>
+    );
+
     return (
         <span className='logo'>
             <span className='cell hidden-xs' onClick={toggleNav}><img src='img/logo.png' /></span>
+            { anchorForMobile }
 
-            <span className='visible-xs-inline pull-left cell' onClick={toggleNav}>
-                <i className='fa fa-bars fa-lg'></i>
-            </span>
-
-            <span className='visible-xs-inline pull-left cell' onClick={toggleNav}>
-                <i className='fa fa-long-arrow-left fa-lg'></i>
-            </span>
         </span>
 
     );
